f07e8bf1dc11903b6edb4dd5b5de8a1e9bcdc70b
Wang, Jia W <jia.w.wang@intel.com>
2018-05-02 13:32:28 +0800

    update firmware binary name
    Signed-off-by: Wang, Jia W <jia.w.wang@intel.com>

--- functionality_test/verify_sof_firmware_presence.sh
+++ functionality_test/verify_sof_firmware_presence.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Verify DSP firmware is presence
-ls /lib/firmware/intel/reef-byt.ri > /dev/null
+ls /lib/firmware/intel/sof-byt.ri > /dev/null
 if [ $? != 0 ]; then
 	echo "Fail: DSP firmware doesnot presence."
 	exit 1
