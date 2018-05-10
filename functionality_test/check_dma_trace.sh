ba57a83d7d0b7603aa1369d00420dd32c960d983
keqiao.zhang <keqiao.zhang@intel.com>
2018-05-10 10:40:59 +0800

    update the rmbox test size

--- functionality_test/check_dma_trace.sh
+++ functionality_test/check_dma_trace.sh
@@ -1,9 +1,9 @@
 # check dma trace output
-rmbox -t > rmbox.log & sleep 2; pkill -9 rmbox
+rmbox -t > rmbox.log & sleep 2 ; pkill -9 rmbox
 # get size of trace log
 size=`du -k rmbox.log |awk '{print $1}'`
 
-if [ $size -gt 10 ]; then
+if [ $size -gt 1 ]; then
 	exit 0
 else
 	exit 1
