64754a5c9243b8eebbc47528d34a156ac92dcfef
Zhang Keqiao <keqiao.zhang@linux.intel.com>
2018-05-15 14:46:43 +0800

    add platform detection in firmware check
    Signed-off-by: Zhang Keqiao <keqiao.zhang@linux.intel.com>

--- functionality_test/verify_sof_firmware_presence.sh
+++ functionality_test/verify_sof_firmware_presence.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+BIOS_VER=`dmidecode -t bios |grep Version |awk -F : '{print $2}' |cut -b 2-4`
+if [ $BIOS_VER == "MNW" ]; then
+	PLATFORM="byt"
+elif [ $BIOS_VER == "APL" ] || [ $BIOS_VER == "UPA" ]; then
+	PLATFORM="apl"
+elif [ $BIOS_VER == "CNL" ]; then
+	PLATFORM="cnl"
+fi
+
 # Verify DSP firmware is presence
-ls /lib/firmware/intel/sof-byt.ri > /dev/null
+ls /lib/firmware/intel/sof-$PLATFORM.ri > /dev/null
 if [ $? != 0 ]; then
 	echo "Fail: DSP firmware doesnot presence."
 	exit 1
